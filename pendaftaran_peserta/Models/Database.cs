c187f20d1bdb2179353f144cfff4670eb00bf120
qoyyum86 <muhammadqoyyum2003@gmail.com>
2025-05-23 20:14:54 +0700

    Add files via upload

---
+++ pendaftaran_peserta/Models/Database.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace pendaftaran_peserta
+{
+    public static class Database
+    {
+        private static string connString = "Server=localhost;Database=db_pencaksilat;Uid=root;Pwd=;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connString);
+        }
+    }
+}
+ 
